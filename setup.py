57eab963054fe59e7823732a615add5e01add8de
David Seaward <david@librem.one>
2022-04-14 21:04:33 +0100

    Build and release tweaks.
    Signed-off-by: David Seaward <david@librem.one>

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
-# trimgmi
-# Copyright 2022 David Seaward
 # SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: Copyright 2022 David Seaward and contributors
 
-from setuptools import setup
+import setuptools
 
-setup(
-    name="trimgmi",
-    version="0.1",
-    description="Opinionated gemtext output",
-    url="https://example.com",
-    author="David Seaward",
-    author_email="david@librem.one",
-    license="GPL-3.0-or-later",
-    packages=["trimgmi"],
-    zip_safe=False,
-)
+setuptools.setup()
